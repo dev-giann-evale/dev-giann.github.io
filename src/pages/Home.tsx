b3b7966ef71dba13007af9acce8b9aabd7103a82
Giann Evale <giann.evale@netzontech.com>
2025-10-15 20:13:20 +0800

    chore: basic themes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme toggle */}
+            <ThemeToggle/>
+            {/* Background Effects */}
+            {/* Navbar */}
+            {/* Main Content */}
+            {/* Footer */}
+        </div>
+    );
+};
