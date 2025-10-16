83efd425353e9e0a19ccdda7b9e845a4ddee3083
Giann Evale <giann.evale@netzontech.com>
2025-10-17 00:03:00 +0800

    chore: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,13 @@
 export const Footer = () => {
     return (
-        <div className="fixed inset-0 overflow hidden pointer-events-none z-0">
-        </div>
+        <footer className="py-12 px-4 bg-card relative border-t mt-12 pt-8 flex flex-wrap justify-between items-center">
+            <p className="text-sm text-muted-foreground">
+                <i className="bi bi-c-circle"></i> {new Date().getFullYear()} Giann-Dev, All Rights Reserved
+            </p>
+            <a href="#hero" 
+                className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
+                <i className="bi bi-arrow-up-circle-fill"></i>
+            </a>
+        </footer>
     );
 };
